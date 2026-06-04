20e3f1a0d5137eb84e9e380c9c3fbd5972d6c5e0
Madison Bryant <madison.bryant006@gmail.com>
2026-06-04 12:47:09 -0400

    Image Encoder & Decoder complete

--- audio_encoder_decoder.py
+++ audio_encoder_decoder.py
@@ -1,10 +1,8 @@
-def encode_audio():
-    #encode audio uploaded by user
-    aud = input("Enter Audio file (w/ Extension): ")
-    audio = AUDIO.open(aud, 'r')
-    text = input("Phrase to be encoded: ")
 
-    newAud = audio.copy()
+
+def encode_audio():
+    # encode audio uploaded by user
+
 
 def decode_audio():
-    #decode audio uploaded by user
+    # decode audio uploaded by user
